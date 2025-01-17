65a05afc44d13f650d2eed891d951659891614f3
MAC <mac@Apples-MacBook-Air.local>
2025-01-17 12:08:39 +0530

    fix ui issues

--- src/Pages/Products/index.jsx
+++ src/Pages/Products/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Products = () => {
-  return <div className=" px-4 py-20">Products</div>;
+  return <div className="px-4 pt-[82px] md:pt-[120px]">Products</div>;
 };
 
 export default Products;
